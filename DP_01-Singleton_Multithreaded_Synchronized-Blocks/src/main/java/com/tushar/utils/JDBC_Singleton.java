e547b0fc174efa83aa5285ce1e2ad426bc558e2e
Tushar <Tushar@Tushar.mshome.net>
2022-01-01 14:16:01 +0530

    Singleton class: using Synchronized blocks with double null check

---
+++ DP_01-Singleton_Multithreaded_Synchronized-Blocks/src/main/java/com/tushar/utils/JDBC_Singleton.java
@@ -0,0 +1,26 @@
+package com.tushar.utils;
+
+public class JDBC_Singleton {
+
+	private static JDBC_Singleton INSTANCE;
+	
+	private JDBC_Singleton() {
+		System.out.println("JDBC_Singleton.JDBC_Singleton()  :: constructor");
+	}
+	
+	public static JDBC_Singleton getInstance() {
+		
+		if(INSTANCE == null) {
+
+			synchronized (JDBC_Singleton.class) //Multi-Threading issue
+			{
+				if(INSTANCE == null) {
+					INSTANCE = new JDBC_Singleton();
+				}//if(INSTANCE == null) 
+			}//synchronized
+			
+		}//if(INSTANCE == null)
+		return INSTANCE;
+	}
+	
+}
