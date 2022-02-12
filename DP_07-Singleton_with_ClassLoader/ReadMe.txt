d07474fe5b40d6516c6234c626f5589318a3d6a9
Tushar <Tushar@Tushar.mshome.net>
2022-02-12 15:21:07 +0530

    ClassLoader Problem with singleton class

---
+++ DP_07-Singleton_with_ClassLoader/ReadMe.txt
@@ -0,0 +1,5 @@
+To test ClassLoader issue in Singleton behaviour, Export any singleton class as jar file.
+and mention in URLClassLoader.
+
+Run ClassLoader_Singleton_Problem.java & observer hashcodes.
+Run ClassLoader_Singleton_Solution.java & observer hashcodes.
---
+++ DP_07-Singleton_with_ClassLoader/src/main/java/com/tushar/test/ClassLoader_Singleton_Solution.java
@@ -0,0 +1,35 @@
+package com.tushar.test;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import com.tushar.utils.JDBC_Singleton;
+
+public class ClassLoader_Singleton_Solution {
+
+	public static void main(String[] args) {
+	
+	try{
+		JDBC_Singleton singleton = JDBC_Singleton.getInstance();	
+		System.out.println("currect proj class singleton hashCode:"+singleton.hashCode());
+		System.out.println("currect proj class ClassLoader:"+singleton.getClass().getClassLoader());
+		
+		//load external jar using  URLClassLoader
+		URLClassLoader loader = new URLClassLoader(new URL[] {new URL("File:\\E:\\c\\Design Patters JAVA\\SingletonClassLoaderTestJar.jar")}, 
+				singleton.getClass().getClassLoader());//parent class loader.
+		
+		//get Class & invoke method to get hashcode
+		Class<?> clazz =loader.loadClass("com.tushar.utils.JDBC_Singleton");
+		Method method = clazz.getDeclaredMethod("getInstance", new Class[] {});
+		Object obj= method.invoke(null);
+		System.out.println("URLClassLoader proj class singleton hashCode:"+obj.hashCode());
+		
+		
+	}//try
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+	}
+
+}
